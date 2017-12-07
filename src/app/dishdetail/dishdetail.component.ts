128025c3639d3e153d6b118c768d0d185ff83d55
Ahmed Mostafa <engahmed.mostafa@yahoo.com>
2017-12-07 08:02:17 +0300

    starting with task 1 assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {Dish} from '../shared/dish';
 import { Params,ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
+import { FormBuilder,FormGroup,Validator,Validators } from "@angular/forms";
+
 
 import 'rxjs/add/operator/switchMap';
 import { DishService } from "../services/dish.service";
+import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.css']
+  styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
   
@@ -17,7 +20,29 @@ export class DishdetailComponent implements OnInit {
    dishIds: number[];
    prev: number;
    next: number;
-  constructor(private dishService:DishService,private location:Location,private route:ActivatedRoute) { }
+
+   commentForm:FormGroup;
+   comment:Comment;
+   formErrors={
+    'writername':'',
+    'commenttext':''
+  };
+
+  validationMessages = {
+    'writername': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+      'maxlength':     'Name cannot be more than 25 characters long.'
+    },
+    'commenttext': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    }
+  };
+  constructor(private dishService:DishService,private location:Location,private route:ActivatedRoute,private fb:FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit() {
 
@@ -36,5 +61,38 @@ export class DishdetailComponent implements OnInit {
 
   goBack():void{
     this.location.back();
+  }
+
+  onSubmit():void{
+
+  }
+
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      writername:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      commenttext:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]]
+
+    });
+    this.commentForm.valueChanges
+    .subscribe(data=>this.onValueChanged(data));
+
+    this.onValueChanged();// reset validation
+  }
+
+  onValueChanged(data?: any):void{
+    if(!this.commentForm){return;}
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 }
